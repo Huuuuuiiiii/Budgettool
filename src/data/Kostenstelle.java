d50f549255660df7808668942adcfb6685aa4b17
Joel Weissenberger <joel.weissenberger@gibmit.ch>
2017-03-19 16:39:47 +0100

    generate Setter and Getter

--- src/data/Kostenstelle.java
+++ src/data/Kostenstelle.java
@@ -2,8 +2,45 @@ package data;
 
 public class Kostenstelle {
 
-	public Kostenstelle() {
-		// TODO Auto-generated constructor stub
+	private String name, kathegorie, datum, gruppe;
+	
+	public Kostenstelle(String name, String kathegorie, String datum, String gruppe) {
+		this.name = name;
+		this.kathegorie = kathegorie;
+		this.datum = datum;
+		this.gruppe = gruppe;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getKathegorie() {
+		return kathegorie;
+	}
+
+	public void setKathegorie(String kathegorie) {
+		this.kathegorie = kathegorie;
+	}
+
+	public String getDatum() {
+		return datum;
+	}
+
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+
+	public String getGruppe() {
+		return gruppe;
+	}
+
+	public void setGruppe(String gruppe) {
+		this.gruppe = gruppe;
 	}
 
 }
