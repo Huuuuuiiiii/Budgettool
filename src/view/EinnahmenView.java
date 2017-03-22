c947bf156df8c6e55e9f4fcaac40b28bb1200503
berufsschule <berufsschule@localhost>
2017-03-22 09:08:22 +0100

    Repaint der comboboxen Person & Kategorie

--- src/view/EinnahmenView.java
+++ src/view/EinnahmenView.java
@@ -33,7 +33,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class EinnahmenView extends JPanel implements ActionListener{
+public class EinnahmenView extends JPanel implements ActionListener {
 
 	private JButton cancelButton, addButton;
 	private JPanel editPanel, titelPanel;
@@ -41,109 +41,118 @@ public class EinnahmenView extends JPanel implements ActionListener{
 	private JLabel betragLabel, datumLabel, personLabel, kategorieLabel, titel;
 	private JTextField betragTextField;
 	private JSpinner dateSpinner, monthSpinner, yearSpinner;
-	private JComboBox<String> betragComboBox, personComboBox, kategorieComboBox;
+	private static JComboBox<String> betragComboBox, personComboBox, kategorieComboBox;
 	private CsvWriter writer = new CsvWriter("Einnahmen");
 
 	/**
 	 * Create the panel.
 	 */
 	public EinnahmenView(ViewHandler mainPanel) {
-		
+
 		this.mainPanel = mainPanel;
-		
+
 		setLayout(new BorderLayout(0, 0));
-        setBorder(new EmptyBorder(10, 10, 10, 10));
-        setBackground(Farben.getDefaultBackgroundcolor());
-        setSize(600, 600);
-        titelPanel = new JPanel();
-        add(titelPanel, BorderLayout.NORTH);
-        
-        titelPanel.setLayout(new BorderLayout(0, 0));
-        titelPanel.setBackground(Farben.getDefaultBackgroundcolor());
-        
-        titel = new JLabel("Einnahmen");
-        titel.setFont(Schrift.TITEL);
-        titel.setHorizontalAlignment(0);
-        titel.setBackground(Farben.getDefaultBackgroundcolor());
-        titel.setForeground(Farben.getDefaultFontcolor());
-        titelPanel.add(titel, BorderLayout.CENTER);
-        
-        cancelButton = Components.createButtom("< Zurück");
-        titelPanel.add(cancelButton,  BorderLayout.WEST);
-        cancelButton.addActionListener(this);
-        
-        editPanel = new JPanel();
-        add(editPanel, BorderLayout.CENTER);
-        editPanel.setBackground(Farben.getDefaultBackgroundcolor());
-        editPanel.setLayout(null);
-        
-        betragLabel = Components.createImageLabel("pic/money.png", 67, 83, 40, 40);
-        editPanel.add(this.betragLabel);
-        
-        kategorieLabel = Components.createImageLabel("pic/folder.png", 67, 133, 40, 40);
-        editPanel.add(this.kategorieLabel);
-        
-        datumLabel = Components.createImageLabel("pic/calendar.png", 67, 183, 40, 40);
-        editPanel.add(this.datumLabel);
-        
-        personLabel = Components.createImageLabel("pic/person.png", 67, 233, 40, 40);
-        editPanel.add(this.personLabel);
-        
-        betragTextField = Components.createTextField("", 147, 92, 116, 20);
-        editPanel.add(this.betragTextField);
-        
-        betragComboBox = Components.createComboBox(273, 92, 66, 20);
-        WaerungReader waerung = new WaerungReader();
-        for(Waerung option : waerung.getOptions()){
-        	betragComboBox.addItem(option.getAbkuerzung());
-        }
-        editPanel.add(this.betragComboBox);
-        
-        kategorieComboBox = Components.createComboBox(147, 142, 155, 20);
-        listOptions("Kategorie", kategorieComboBox);
-        editPanel.add(this.kategorieComboBox);
-        
-        dateSpinner = Components.createSpinner(147, 192, 100, 20);
-        editPanel.add(this.dateSpinner);
-        
-//        monthSpinner = Components.createSpinner(197, 192, 40, 20);
-//        editPanel.add(this.monthSpinner);
-        
-//        yearSpinner = Components.createSpinner(247, 192, 55, 20);
-//        editPanel.add(this.yearSpinner);
-        
-        personComboBox= Components.createComboBox(148, 242, 154, 20);
-        listOptions("Person", personComboBox);
-        editPanel.add(this.personComboBox);
-        
-        addButton = Components.createButtom("Enter");
-        addButton.setBounds(250, 292, 89, 23);
-        addButton.setBorder(Rahmen.roundedBorder);
-        addButton.addActionListener(this);
-        editPanel.add(this.addButton);
-    }
+		setBorder(new EmptyBorder(10, 10, 10, 10));
+		setBackground(Farben.getDefaultBackgroundcolor());
+		setSize(600, 600);
+		titelPanel = new JPanel();
+		add(titelPanel, BorderLayout.NORTH);
+
+		titelPanel.setLayout(new BorderLayout(0, 0));
+		titelPanel.setBackground(Farben.getDefaultBackgroundcolor());
+
+		titel = new JLabel("Einnahmen");
+		titel.setFont(Schrift.TITEL);
+		titel.setHorizontalAlignment(0);
+		titel.setBackground(Farben.getDefaultBackgroundcolor());
+		titel.setForeground(Farben.getDefaultFontcolor());
+		titelPanel.add(titel, BorderLayout.CENTER);
+
+		cancelButton = Components.createButtom("< Zurück");
+		titelPanel.add(cancelButton, BorderLayout.WEST);
+		cancelButton.addActionListener(this);
+
+		editPanel = new JPanel();
+		add(editPanel, BorderLayout.CENTER);
+		editPanel.setBackground(Farben.getDefaultBackgroundcolor());
+		editPanel.setLayout(null);
+
+		betragLabel = Components.createImageLabel("pic/money.png", 67, 83, 40, 40);
+		editPanel.add(this.betragLabel);
+
+		kategorieLabel = Components.createImageLabel("pic/folder.png", 67, 133, 40, 40);
+		editPanel.add(this.kategorieLabel);
+
+		datumLabel = Components.createImageLabel("pic/calendar.png", 67, 183, 40, 40);
+		editPanel.add(this.datumLabel);
+
+		personLabel = Components.createImageLabel("pic/person.png", 67, 233, 40, 40);
+		editPanel.add(this.personLabel);
+
+		betragTextField = Components.createTextField("", 147, 92, 116, 20);
+		editPanel.add(this.betragTextField);
+
+		betragComboBox = Components.createComboBox(273, 92, 66, 20);
+		WaerungReader waerung = new WaerungReader();
+		for (Waerung option : waerung.getOptions()) {
+			betragComboBox.addItem(option.getAbkuerzung());
+		}
+		editPanel.add(this.betragComboBox);
+
+		kategorieComboBox = Components.createComboBox(147, 142, 155, 20);
+		listOptions("Kategorie", kategorieComboBox);
+		editPanel.add(this.kategorieComboBox);
+
+		dateSpinner = Components.createSpinner(147, 192, 100, 20);
+		editPanel.add(this.dateSpinner);
+
+		// monthSpinner = Components.createSpinner(197, 192, 40, 20);
+		// editPanel.add(this.monthSpinner);
+
+		// yearSpinner = Components.createSpinner(247, 192, 55, 20);
+		// editPanel.add(this.yearSpinner);
+
+		personComboBox = Components.createComboBox(148, 242, 154, 20);
+		listOptions("Person", personComboBox);
+		editPanel.add(this.personComboBox);
+
+		addButton = Components.createButtom("Enter");
+		addButton.setBounds(250, 292, 89, 23);
+		addButton.setBorder(Rahmen.roundedBorder);
+		addButton.addActionListener(this);
+		editPanel.add(this.addButton);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if (e.getSource() == cancelButton){
+
+		if (e.getSource() == cancelButton) {
 			this.setVisible(false);
 			System.out.println();
 			mainPanel.getMainViewPanel().setVisible(true);
 			mainPanel.getEinnahmeViewPanel().setVisible(false);
 		}
-		if (e.getSource() == addButton){
-			writer.writer(new Kostenstelle(betragTextField.getText(), kategorieComboBox.getSelectedItem().toString(), ((Date) dateSpinner.getModel().getValue()), personComboBox.getSelectedItem().toString()));
+		if (e.getSource() == addButton) {
+			writer.writer(new Kostenstelle(betragTextField.getText(), kategorieComboBox.getSelectedItem().toString(),
+					((Date) dateSpinner.getModel().getValue()), personComboBox.getSelectedItem().toString()));
 			betragTextField.setText("");
 			this.setVisible(false);
 			mainPanel.getMainViewPanel().setVisible(true);
+//			//listOptions("Person", personComboBox);
 		}
 	}
-	
-	private void listOptions(String Filename, JComboBox<String> ComboBox){
-		 DropdownReader reader = new DropdownReader(Filename);
-	     for(String option : reader.getOptions()){
-	    	 ComboBox.addItem(option);
-	     }  
+
+	private static void listOptions(String Filename, JComboBox<String> ComboBox) {
+		DropdownReader reader = new DropdownReader(Filename);
+		for (String option : reader.getOptions()) {
+			ComboBox.addItem(option);
+		}
+	}
+
+	public static void uploadPerson() {
+		listOptions("Person", personComboBox);
+	}
+	public static void uploadKategory(){
+		listOptions("Kategorie", kategorieComboBox);
 	}
 }
--- src/view/Settings.java
+++ src/view/Settings.java
@@ -1,4 +1,5 @@
 package view;
+
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
@@ -29,18 +30,19 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 
-
 /*
  * Copyright 2017 Coop, Informatik / Internet Entwicklung.
  * Use is subject to license terms.
  */
 
-public class Settings extends JPanel implements ActionListener{
+public class Settings extends JPanel implements ActionListener {
 
-	private JPanel titelPanel, settingsPanel, settingsLayoutPanel, settingsAddPanel, settingsBudgetPanel, settingsKathegoriePanel;
+	private JPanel titelPanel, settingsPanel, settingsLayoutPanel, settingsAddPanel, settingsBudgetPanel,
+			settingsKathegoriePanel;
 	private JButton cancelButton, personButton, kategorieButton;
 	private JSplitPane splitPane;
-	private JLabel darkdesignLabel, personLabel, namePersonLabel, waerungLabel, finazMothLabel, mothBudgetLabel, zyklusLabel, kategorieNameLabel, kategorieLabel, Titel;
+	private JLabel darkdesignLabel, personLabel, namePersonLabel, waerungLabel, finazMothLabel, mothBudgetLabel,
+			zyklusLabel, kategorieNameLabel, kategorieLabel, Titel;
 	private JToggleButton darkdesignToggleButton, settingsBudgetButton, settingsViewButton, settingsAddButton;
 	private JTextField personTextField, kategorieTextField, mothBudgetTextField;
 	private JComboBox<String> zyklusComboBox, mothBudgetComboBox, waerungComboBox;
@@ -54,197 +56,203 @@ public class Settings extends JPanel implements ActionListener{
 	 * Create the panel.
 	 */
 	public Settings(ViewHandler mainPanel) {
-		
+
 		this.mainPanel = mainPanel;
 		setBackground(Farben.getDefaultBackgroundcolor());
 		setLayout(new BorderLayout(0, 0));
 		setBorder(new EmptyBorder(10, 10, 10, 10));
-		
+
 		titelPanel = new JPanel();
 		add(titelPanel, BorderLayout.NORTH);
 		titelPanel.setLayout(new BorderLayout(0, 0));
 		titelPanel.setBackground(Farben.getDefaultBackgroundcolor());
 		titelPanel.setForeground(Farben.getDefaultFontcolor());
-		
+
 		Titel = new JLabel("Einstellungen");
 		Titel.setFont(Schrift.TITEL);
 		Titel.setHorizontalAlignment(SwingConstants.CENTER);
 		Titel.setBackground(Farben.getDefaultBackgroundcolor());
 		Titel.setForeground(Farben.getDefaultFontcolor());
 		titelPanel.add(Titel, BorderLayout.CENTER);
-		
+
 		cancelButton = Components.createButtom("< Zurück");
 		titelPanel.add(cancelButton, BorderLayout.WEST);
 		cancelButton.addActionListener(this);
-		
+
 		splitPane = new JSplitPane();
 		splitPane.setBackground(Farben.getDefaultBackgroundcolor());
 		splitPane.setBorder(null);
 		add(splitPane);
-		
+
 		settingsPanel = new JPanel();
 		settingsPanel.setBackground(Farben.getDefaultBackgroundcolor());
 		splitPane.setRightComponent(settingsPanel);
 		settingsPanel.setLayout(new CardLayout(0, 0));
-		
+
 		settingsLayoutPanel = new JPanel();
 		settingsLayoutPanel.setBackground(Farben.getDefaultBackgroundcolor());
 		settingsPanel.add(settingsLayoutPanel, "name_3361411458953");
 		settingsLayoutPanel.setLayout(null);
-		
+
 		darkdesignLabel = Components.createLabel("Dark Design", 20, 36, 100, 14);
 		settingsLayoutPanel.add(darkdesignLabel);
-		
+
 		darkdesignToggleButton = Components.createToggleButton("ON / OF");
 		darkdesignToggleButton.setBounds(122, 29, 128, 23);
 		settingsLayoutPanel.add(darkdesignToggleButton);
-		
+
 		settingsAddPanel = new JPanel();
 		settingsAddPanel.setBackground(Farben.getDefaultBackgroundcolor());
 		settingsPanel.add(settingsAddPanel, "name_3363203520759");
 		settingsAddPanel.setLayout(null);
-		
+
 		personLabel = Components.createLabel("Person", 10, 11, 110, 14);
 		settingsAddPanel.add(personLabel);
-		
+
 		namePersonLabel = Components.createLabel("Name", 20, 36, 100, 14);
 		settingsAddPanel.add(namePersonLabel);
-		
+
 		personTextField = Components.createTextField("", 122, 33, 128, 20);
 		settingsAddPanel.add(personTextField);
-		
+
 		personButton = Components.createButtom("Person hinzufügen");
-		personButton.setBounds(30,60,155,20);
+		personButton.setBounds(30, 60, 155, 20);
 		personButton.setBorder(Rahmen.roundedBorder);
 		personButton.addActionListener(this);
 		settingsAddPanel.add(personButton);
-		
+
 		kategorieLabel = Components.createLabel("Kategorie", 10, 125, 110, 14);
 		kategorieLabel.setFont(Schrift.STANDART);
 		settingsAddPanel.add(kategorieLabel);
-		
+
 		kategorieNameLabel = Components.createLabel("Kategorie", 20, 150, 100, 14);
 		settingsAddPanel.add(kategorieNameLabel);
-		
+
 		kategorieTextField = Components.createTextField("", 122, 147, 128, 20);
 		settingsAddPanel.add(kategorieTextField);
-		
+
 		kategorieButton = Components.createButtom("Kategorie hinzufügen");
 		kategorieButton.setBounds(20, 175, 155, 20);
 		kategorieButton.setBorder(Rahmen.roundedBorder);
 		kategorieButton.addActionListener(this);
 		settingsAddPanel.add(kategorieButton);
-		
+
 		settingsBudgetPanel = new JPanel();
 		settingsBudgetPanel.setBackground(Farben.getDefaultBackgroundcolor());
 		settingsPanel.add(settingsBudgetPanel, "name_3368500105902");
 		settingsBudgetPanel.setLayout(null);
-		
+
 		waerungLabel = Components.createLabel("Währung", 20, 36, 100, 14);
 		settingsBudgetPanel.add(waerungLabel);
-		
+
 		mothBudgetLabel = Components.createLabel("Monatsbudget", 20, 76, 100, 14);
 		settingsBudgetPanel.add(mothBudgetLabel);
-		
+
 		finazMothLabel = Components.createLabel("Finazmonatsbeginn", 20, 116, 100, 14);
 		settingsBudgetPanel.add(finazMothLabel);
-		
+
 		zyklusLabel = Components.createLabel("Zyklus", 20, 156, 100, 14);
 		settingsBudgetPanel.add(zyklusLabel);
-		
+
 		waerungComboBox = Components.createComboBox(122, 33, 62, 20);
-	    for(Waerung option : waerung.getOptions()){
-	      	waerungComboBox.addItem(option.getAbkuerzung());
-	    }
+		for (Waerung option : waerung.getOptions()) {
+			waerungComboBox.addItem(option.getAbkuerzung());
+		}
 		settingsBudgetPanel.add(waerungComboBox);
-		
+
 		mothBudgetTextField = Components.createTextField("", 122, 73, 86, 20);
 		settingsBudgetPanel.add(mothBudgetTextField);
-		
+
 		finazMothSpinner = Components.createSpinner(122, 113, 86, 20);
 		settingsBudgetPanel.add(finazMothSpinner);
-		
+
 		mothBudgetComboBox = Components.createComboBox(218, 73, 62, 20);
-		for(Waerung option : waerung.getOptions()){
+		for (Waerung option : waerung.getOptions()) {
 			mothBudgetComboBox.addItem(option.getAbkuerzung());
-	    }
+		}
 		settingsBudgetPanel.add(mothBudgetComboBox);
-		
+
 		zyklusComboBox = Components.createComboBox(122, 153, 86, 20);
-		zyklusComboBox.addItem("T�glich");
-		zyklusComboBox.addItem("W�chentlich");
+		zyklusComboBox.addItem("Täglich");
+		zyklusComboBox.addItem("Wöchentlich");
 		zyklusComboBox.addItem("Monatlich");
-		zyklusComboBox.addItem("J�hrlich");
+		zyklusComboBox.addItem("Jährlich");
 		settingsBudgetPanel.add(zyklusComboBox);
-		
+
 		settingsKathegoriePanel = new JPanel();
 		splitPane.setLeftComponent(settingsKathegoriePanel);
 		settingsKathegoriePanel.setBackground(Farben.getDefaultBackgroundcolor());
 		settingsKathegoriePanel.setLayout(new GridLayout(10, 1, 0, 0));
-		
-		settingsViewButton = Components.createToggleButton("View");
+
+		settingsViewButton = Components.createToggleButton("Ansicht");
 		settingsViewButton.setBorder(Rahmen.defaultBorder);
 		settingsKathegoriePanel.add(settingsViewButton);
 		settingsViewButton.addActionListener(this);
-		
-		settingsAddButton = Components.createToggleButton("Add");
+
+		settingsAddButton = Components.createToggleButton("Hinzufügen");
 		settingsAddButton.setBorder(Rahmen.defaultBorder);
 		settingsKathegoriePanel.add(settingsAddButton);
 		settingsAddButton.addActionListener(this);
-	
+
 		settingsBudgetButton = Components.createToggleButton("Budget");
 		settingsBudgetButton.setBorder(Rahmen.defaultBorder);
 		settingsKathegoriePanel.add(settingsBudgetButton);
 		settingsBudgetButton.addActionListener(this);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if (e.getSource() == cancelButton){
+
+		if (e.getSource() == cancelButton) {
 			this.setVisible(false);
 			mainPanel.getMainViewPanel().setVisible(true);
 		}
-		
-		if (e.getSource() == settingsAddButton){
+
+		if (e.getSource() == settingsAddButton) {
 			setSettingsViewPanelVisible(false);
 			setsettingsBudgetPanelVisible(false);
 			setsettingsAddPanelVisible(true);
 		}
-		
-		else if (e.getSource() == settingsViewButton){
+
+		else if (e.getSource() == settingsViewButton) {
 			setSettingsViewPanelVisible(true);
 			setsettingsBudgetPanelVisible(false);
 			setsettingsAddPanelVisible(false);
 		}
-		
-		else if (e.getSource() == settingsBudgetButton){
+
+		else if (e.getSource() == settingsBudgetButton) {
 			setSettingsViewPanelVisible(false);
 			setsettingsBudgetPanelVisible(true);
 			setsettingsAddPanelVisible(false);
 		}
-		
+
 		if (e.getSource() == personButton) {
 			personWriter.writer(personTextField.getText());
 			personTextField.setText("");
+			EinnahmenView.uploadPerson();
+			AusgabenView.uploadPerson();
+			ShortcutView.uploadPerson();
 		}
 		if (e.getSource() == kategorieButton) {
 			kategorieWriter.writer(kategorieTextField.getText());
 			kategorieTextField.setText("");
+			EinnahmenView.uploadKategory();
+			AusgabenView.uploadKategory();
+			ShortcutView.uploadKategory();
 		}
 	}
-	
-	private void setSettingsViewPanelVisible(boolean visible){
+
+	private void setSettingsViewPanelVisible(boolean visible) {
 		settingsLayoutPanel.setVisible(visible);
 		settingsViewButton.setSelected(visible);
 	}
-	
-	private void setsettingsAddPanelVisible(boolean visible){
+
+	private void setsettingsAddPanelVisible(boolean visible) {
 		settingsAddPanel.setVisible(visible);
 		settingsAddButton.setSelected(visible);
 	}
-	
-	private void setsettingsBudgetPanelVisible(boolean visible){
+
+	private void setsettingsBudgetPanelVisible(boolean visible) {
 		settingsBudgetPanel.setVisible(visible);
 		settingsBudgetButton.setSelected(visible);
 	}
